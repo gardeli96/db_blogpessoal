6eaedcb6759d1d3fd985ef52cde10b83415442d0
daniel <gardeli96>
2022-05-25 18:12:34 -0300

    Inplementação Deploy

--- src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
+++ src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.generation.blogpessoal.configuration;
 
 import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -10,7 +12,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
-
+@Configuration
 public class SwaggerConfig {
 
     @Bean
